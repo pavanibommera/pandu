c035eaec72dedc95c1cdd12e4f2fd28711da3f44
PAVANI B <PAVANI B@LAPTOP-7VMGP23T>
2022-02-04 16:21:42 +0530

    fourth commit

---
+++ javaspecifiers/src/javaspecifiers/Example1.java
@@ -0,0 +1,23 @@
+package com.javaspecifiers;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		/**
+		 * Write a java program to print customer infomation(id,name,address,salary and age)
+		 */
+		int CustomerId=1621;
+		String CustomerName="Ganesh";
+		String CustomerAddress="Armoor";
+		int CustomerSalary=20000;
+		int Age=22;
+		System.out.println(CustomerId);
+		System.out.println(CustomerName);
+		System.out.println(CustomerAddress);
+		System.out.println(CustomerSalary);
+		System.out.println(CustomerAge);
+		
+
+	}
+
+}
